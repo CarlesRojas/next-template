b5669a140688adef9eb064c746515c80fbfeca4a
Carles <carles.rojas@mundimoto.com>
2022-06-14 12:27:37 +0200

    test: check prettier again x3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,6 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-
-
-
-    
     return <Component {...pageProps} />;
 }
 

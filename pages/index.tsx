48259b1f757fca67bdc777dea5af396aae2213e9
Carles <carles.rojas@mundimoto.com>
2022-06-14 16:59:52 +0200

    fix: move template to a component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,8 @@
 import type { NextPage } from "next";
-import { Button } from "primereact/button";
-import { ColorPicker } from "primereact/colorpicker";
-import { useThemeContext } from "../context/Theme";
+import Template from "../components/Template";
 
 const Home: NextPage = () => {
-    const { themeState, themeActions } = useThemeContext();
-
-    const { accentColor, dark } = themeState;
-    const { switchAccentColor, switchToLightMode, switchToDarkMode } = themeActions;
-
-    const style = {
-        "--accent-color": `#${accentColor}`,
-        backgroundColor: dark ? "black" : "white",
-        color: dark ? "white" : "black",
-    } as const;
-
-    return (
-        <div className="h-full flex flex-column justify-content-center align-items-center flex-grow-1" style={style}>
-            <h1 className={"mb-5"} style={{ color: "var(--accent-color)" }}>
-                NextJS Template
-            </h1>
-
-            {dark ? (
-                <Button label="Switch to Light Mode" onClick={switchToLightMode} className="p-button-secondary  mb-5" />
-            ) : (
-                <Button label="Switch to Dark Mode" onClick={switchToDarkMode} className="p-button-secondary  mb-5" />
-            )}
-
-            <p className="mb-2">Change Accent Color:</p>
-            <ColorPicker value={accentColor} onChange={(e) => switchAccentColor(e.value as string)} />
-        </div>
-    );
+    return <Template />;
 };
 
 export default Home;
